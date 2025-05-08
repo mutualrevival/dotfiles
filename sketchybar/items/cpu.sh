9a01e9813f9c6df42d5b6b181c9a4eb90ec0f957
mutualrevival <classic-deluxe@proton.me>
2025-05-08 13:44:47 -0400

    sketchybar config updates

--- sketchybar/items/cpu.sh
+++ sketchybar/items/cpu.sh
@@ -3,10 +3,13 @@
 source "$CONFIG_DIR/colors.sh"
 
 sketchybar --add item cpu left \
-           --set cpu  update_freq=2 \
-                      icon=􀧓  \
-                      background.height=2 \
-                      background.color=$COLOR_RED \
-                      background.y_offset=-15 \
-                      background.corner_radius=0 \
-                      script="$PLUGIN_DIR/cpu.sh" \
+           --set cpu update_freq=2 \
+                     icon=􀧓 \
+                     label.font="SF Pro:SemiBold:12" \
+                     label.align=right \
+                     label.width=30 \
+                     background.height=2 \
+                     background.color=$COLOR_RED \
+                     background.y_offset=-15 \
+                     background.corner_radius=0 \
+                     script="$PLUGIN_DIR/cpu.sh"
