16067135c45c8ad8aa2259e7312809a688892d59
mutualrevival <classic-deluxe@proton.me>
2025-05-09 00:05:03 -0400

    sketchybar config updates

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -2,7 +2,7 @@
 
 source "$CONFIG_DIR/colors.sh"
 
-sketchybar --add item front_app q \
+sketchybar --add item front_app left \
            --set front_app       background.color=$ITEM_BG_COLOR \
                                  icon.color=$COLOR_BLACK \
                                  icon.font="sketchybar-app-font:Regular:12.0" \
