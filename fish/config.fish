6e8c76c21fc97905740db0825ae3d1185e033777
mutualrevival <classic-deluxe@proton.me>
2025-06-02 20:23:09 -0400

    updated fish config

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,7 @@ set -gx PATH /opt/homebrew/bin $PATH
 # Source Color Config
 source ~/dotfiles/fish/colors.fish
 fish_add_path /Users/jaycsmith/.spicetify
+
+# Suppress welcome message
+set -g fish_greeting
+
